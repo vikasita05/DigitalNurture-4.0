4684cd6d13aa00b2f16837ce158bc4ba7c6476f4
vikasita05 <vikasita21@gmail.com>
2025-07-25 21:29:49 +0530

    Add files via upload

---
+++ JavaFSE/week 6/2. React handsOn/studentapp/src/Components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Contact(){
+    return(
+        <div>
+            <h2>Welcome to the Contact page of the Student Management Portal</h2>
+        </div>
+    );
+}
+export default Contact;
---
+++ JavaFSE/week 6/2. React handsOn/studentapp/src/Components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Home(){
+    return(
+        <div>
+            <h2>Welcome to the Home page of Student Management Portal</h2>
+        </div>
+    );
+}
+export default Home;
